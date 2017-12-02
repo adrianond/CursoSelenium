64b3b45339387a43cf8764f923d9966a87ff29c3
adrianond <adrianond@yahoo.com.br>
2017-12-02 01:00:56 -0200

    Segundo teste GIT

--- src/main/java/br/com/teste/campo/treinamento/OutraClasseTesteGit.java
+++ src/main/java/br/com/teste/campo/treinamento/OutraClasseTesteGit.java
@@ -1,9 +1,9 @@
 package br.com.teste.campo.treinamento;
 
-public class ClasseTesteGit {
+public class OutraClasseTesteGit {
 	
 	public static void main(String[] args) {
-		System.out.println("Teste GIT");
+		
 	}
 
 }
