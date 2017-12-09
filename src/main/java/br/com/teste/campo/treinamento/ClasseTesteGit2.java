82c5a78ae8aac48e243a1100ba20aa7873f4c9b9
adrianond <adrianond@yahoo.com.br>
2017-12-09 17:24:50 -0200

    Merge da branch desenv

--- src/main/java/br/com/teste/campo/treinamento/ClasseTesteGit2.java
+++ src/main/java/br/com/teste/campo/treinamento/ClasseTesteGit2.java
@@ -1,10 +1,11 @@
 package br.com.teste.campo.treinamento;
 
-public class ClasseTesteGit {
+public class ClasseTesteGit2 {
 	
 	public static void main(String[] args) {
 		System.out.println("Teste GIT");
 		System.out.println("Editada direto no github");
+		System.out.println("teste merge");
 	}
 
 }
