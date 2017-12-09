a5cf863f5762038be73b98a7a57ceb755e957ddd
adrianond <adrianond@yahoo.com.br>
2017-12-09 17:09:32 -0200

    Teste de merge

--- src/main/java/br/com/teste/campo/treinamento/ClasseTesteGit2.java
+++ src/main/java/br/com/teste/campo/treinamento/ClasseTesteGit2.java
@@ -1,6 +1,6 @@
 package br.com.teste.campo.treinamento;
 
-public class ClasseTesteGit {
+public class ClasseTesteGit2 {
 	
 	public static void main(String[] args) {
 		System.out.println("Teste GIT");
