b367cf304794ce510679140117d0d7dc8e75dfd4
adrianond <adrianond@yahoo.com.br>
2018-01-07 19:52:10 -0200

    teste

--- src/main/java/br/com/teste/suite/teste/SuiteTestes.java
+++ src/main/java/br/com/teste/suite/teste/SuiteTestes.java
@@ -1,5 +1,19 @@
 package br.com.teste.suite.teste;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import br.com.teste.campo.treinamento.TesteCampoTreinamento;
+import br.com.teste.campo.treinamento.ValidarCampos;
+import br.com.teste.regras.cadastro.TesteRegrasCadastro;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	TesteRegrasCadastro.class,
+	ValidarCampos.class,
+	TesteCampoTreinamento.class
+})
 public class SuiteTestes {
 
 }
